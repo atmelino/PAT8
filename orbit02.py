d7787ef8fb14a3027f9597feedd369dc5288dc42
user <atmelino@github.com>
2016-07-19 23:52:57 -0500

    menu style

--- orbit02.py
+++ orbit02.py
@@ -1,8 +1,8 @@
 #import pandas as pd
 import numpy as np
-import matplotlib.pyplot as plt
 import matplotlib
-
+matplotlib.use('Agg')
+import matplotlib.pyplot as plt
 
 t = np.arange(0.0, 2.0, 0.01)
 s = np.sin(2*np.pi*t)
@@ -10,16 +10,16 @@ s = np.sin(2*np.pi*t)
 
 print 7
 
-plt.plot(t, s)    
-print 8
-plt.xlabel('time (s)')
-plt.ylabel('voltage (mV)')
-plt.title('About as simple as it gets, folks')
-plt.grid(True)
-
-
-
 try:
+    plt.plot(t, s)    
+    print 8
+    plt.xlabel('time (s)')
+    plt.ylabel('voltage (mV)')
+    plt.title('About as simple as it gets, folks')
+    plt.grid(True)
+
+
+
     #plt.savefig("test.png")
     #plt.show()
     plt.savefig("foo.png", bbox_inches='tight')
