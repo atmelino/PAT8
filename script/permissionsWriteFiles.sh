0440858075edde1b601319aa03b245b7f6b6153b
user <atmelino@github.com>
2016-08-06 23:26:55 -0500

    permissions script

---
+++ script/permissionsWriteFiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+
+sudo chmod 777 ../applications/writeFiles
+sudo chmod 777 ../develop/basics/writeFiles
+sudo chmod 777 ../develop/matplotlib/writeFiles
+
+#if that doesn't help, delete the files that need to be overwritten
