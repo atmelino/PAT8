eb3092efec8b8cf599287d2275158260b3254de1
jatnik <jatnik@jat>
2016-08-21 18:44:52 -0500

    constrained opt

--- develop/scipy/optimize/constrainedOpt01.py
+++ develop/scipy/optimize/constrainedOpt01.py
@@ -15,28 +15,28 @@ cons = ({'type': 'eq',
          'fun' : lambda x: np.array([x[1] - (x[0]-1)**4 - 2])})
 
 bnds = ((0.5, 1.5), (1.5, 2.5))
+x0 = [0, 2.5]
 
 ux = opt.minimize(f, x0, constraints=None)
+print ux
 
+cx = opt.minimize(f, x0, bounds=bnds, constraints=cons)
+print cx
 
+xmin=-1
+ymin=-1
 
-x = np.linspace(0, 3, 100)
-y = np.linspace(0, 3, 100)
+x = np.linspace(xmin, 3, 100)
+y = np.linspace(ymin, 3, 100)
 X, Y = np.meshgrid(x, y)
 Z = f(np.vstack([X.ravel(), Y.ravel()])).reshape((100,100))
 plt.contour(X, Y, Z, np.arange(-1.99,10, 1));
 plt.plot(x, x**3, 'k:', linewidth=1)
 plt.plot(x, (x-1)**4+2, 'k:', linewidth=1)
+plt.text(ux['x'][0], ux['x'][1], 'x', va='center', ha='center', size=20, color='blue')
+plt.text(cx['x'][0], cx['x'][1], 'x', va='center', ha='center', size=20, color='red')
 plt.fill([0.5,0.5,1.5,1.5], [2.5,1.5,1.5,2.5], alpha=0.3)
-plt.axis([0,3,0,3])
-
-
-
-
-
-
+plt.axis([xmin,3,ymin,3]);
 
 plt.show()
-
-
 
