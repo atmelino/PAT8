d3a9be45c74e763a574bcd00c2edc1508309c7ac
jatnik <jatnik@jat>
2016-08-23 19:56:50 -0500

    scenarios for earth-moon

---
+++ old/old.js
@@ -0,0 +1,42 @@
+	// print acceleration
+	myParams3 = {
+		py : "earthMoon02.py",
+		central : central,
+		GMearth : 398600.441890000,
+		GMmoon : 0,
+		x0 : -10000,
+		y0 : 0,
+		vx0 : 0,
+		vy0 : 6.3,
+		t0 : 0,
+		deltat : 2500,
+		plotsize : 20000,
+	};
+	myParams1 = {
+			py : "earthMoon02.py",
+			central : central,
+			GMearth : 398600.441890000,
+			GMmoon : 490279,
+			x0 : -6678,
+			y0 : 0,
+			vx0 : 0,
+			vy0 : 10.86,
+			t0 : 390000,
+			deltat : 859200,
+			plotsize : 320000,
+		};
+
+		myParams2 = {
+			py : "earthMoon02.py",
+			central : central,
+			GMearth : 398600.441890000,
+			GMmoon : 49027.9,
+			x0 : -6678,
+			y0 : 0,
+			vx0 : 0,
+			vy0 : 10.82,
+			t0 : 340000,
+			deltat : 220000,
+			plotsize : 320000,
+		};
+
